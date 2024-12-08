371b50f3c7987c6c67ab184d5e153559b70bb7ad
dheidelberger <david.heidelberger@gmail.com>
2024-12-08 01:51:05 -0500

    Refactor diffString

--- API/api.js
+++ API/api.js
@@ -1,20 +1,11 @@
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
 const fs = require('fs');
 require('colors');
+import { diffString } from '../Utilities/dateUtils.js';
 
 let userToHighlight = 'dheidelberger';
 const topXToTimeTrack = 10;
-
-function diffString(timeDiff) {
-    let hDiff = Math.floor(timeDiff / 60 / 60);
-    timeDiff -= hDiff * 60 * 60;
-    let mDiff = Math.floor(timeDiff / 60);
-    timeDiff -= mDiff * 60;
-    hDiff = hDiff.toString().padStart(2, '0');
-    mDiff = mDiff.toString().padStart(2, '0');
-    timeDiff = timeDiff.toString().padStart(2, '0');
-
-    return `(+${hDiff}:${mDiff}:${timeDiff})`;
-}
 
 const players = Object.values(
     JSON.parse(fs.readFileSync('api.json', 'utf-8')).members
