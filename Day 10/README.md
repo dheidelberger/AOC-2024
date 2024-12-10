db0d01cf79ecad28c22c91b451c939430b3c2738
dheidelberger <david.heidelberger@gmail.com>
2024-12-10 00:20:40 -0500

    Day 10

---
+++ Day 10/README.md
@@ -0,0 +1,15 @@
+# Day 10 statistics:
+
+Input Downloaded: 12/10/2024, 12:00:06 AM\
+Part 1 submitted: 12/10/2024, 12:09:15 AM (+00:09:08)\
+Part 2 submitted: 12/10/2024, 12:11:03 AM (+00:01:48)
+
+Part 1 Rank: 922 (0 points)\
+Part 2 Rank: 707 (0 points)
+
+_Note that as of 2024 Day 8, input download happens automatically when I first run the part 1 template file. I do this immediately after opening the puzzle for the first time._
+
+Part 1 Run Time: 13.953ms\
+Part 2 Run Time: 14.589ms
+
+_Code is run on a 2020 M1 Macbook Pro with 16GB of RAM_
---
+++ Day 10/part2.js
@@ -0,0 +1,54 @@
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const colors = require('colors');
+const G = require('generatorics');
+const Heap = require('heap');
+import * as setUtilities from '../Utilities/setUtilities.js';
+import Grid from '../Utilities/Grid.js';
+import { getInput } from '../Utilities/inputUtils.js';
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
+const testInputFile = 'testinput.txt';
+await getInput(__dirname, testInputFile);
+
+console.time('Run Time');
+
+const testMode = false;
+
+let input = fs
+    .readFileSync(testMode ? 'testinput.txt' : 'input.txt', 'utf-8')
+    .trim();
+
+let grid = new Grid(input, { parseAsNumbers: true });
+
+console.log(grid);
+
+let trailheads = grid.getAllCells().filter((cell) => cell.value === 0);
+let solutions = 0;
+let part2 = 0;
+for (let trailhead of trailheads) {
+    let paths = new Set();
+    let toVisit = [trailhead];
+    while (toVisit.length > 0) {
+        const currentSquare = toVisit.pop();
+        let surroundings = currentSquare
+            .getCellInDirections(Grid.cardinalDirections)
+            .filter((cell) => cell.value === currentSquare.value + 1);
+        for (let surrounding of surroundings) {
+            if (surrounding.value === 9) {
+                paths.add(surrounding.key);
+                part2 += 1;
+                continue;
+            }
+            toVisit.push(surrounding);
+        }
+    }
+    console.log(paths);
+    solutions += paths.size;
+}
+console.log(solutions);
+console.log(part2);
+console.timeEnd('Run Time');
